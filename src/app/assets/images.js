2d71be94d410974e649cee745163cc5d892b2d71
Sendoa Barciela <sendoa.barciela@docomodigital.com>
2019-02-08 11:53:59 +0100

    Fix favicon

--- src/app/assets/images.js
+++ src/app/assets/images.js
@@ -1,2 +1,2 @@
-export const VUE_CODE = require('public/assets/img/vue-code.png')
-export const VUE_LOGO = require('public/assets/img/vue-logo.png')
+export const VUE_CODE = require('public/assets/images/vue-code.png')
+export const VUE_LOGO = require('public/assets/images/vue-logo.png')
